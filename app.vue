d67f6250d4cfb9900b7f0aeab8a85262e16273e0
Jean Loberg Hansen <jeanloberg@gmail.com>
2024-11-15 08:39:45 +0100

    doco start

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ content/index.md
@@ -0,0 +1,8 @@
+---
+title: "First Blog Post"
+description: "This is the first post in our blog"
+---
+
+# Hello World
+
+Welcome to our first blog post using `@nuxt/content`.
